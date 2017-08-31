25d3ef90bb50ca44600ccae168d7b895d6cc4f8e
sasikesani <kesanisasikanth@gmail.com>
2017-08-31 17:12:31 -0400

    Auto-generated commit

---
+++ assets/js/pagefooter.js
@@ -0,0 +1,39 @@
+function pagefooter()
+{
+html = 
+'<div class="footer">'+
+'<div class="col-md-2">'+
+'<p>Related Sites</p>'+
+'<p><a href="#">GSA</a></br>'+
+'<a href="#">Auto Auctions</a></br>'+
+'<a href="#">AutoChoice</a></br>'+
+'<a href="#">DRM</a></br>'+
+'<a href="#">F.A.S.T.</a></br>'+
+'</div>'+
+'<div class="col-md-2">'+
+'<p>&nbsp;</p>'+
+'<p><a href="#">Federal Vehicle Standard</a></br>'+
+'<a href="#">FedFMS</a></br>'+
+'<a href="#">VCSS</a></br>'+
+'<a href="#">VFE</a></p>'+
+'</div>'+
+'<div class="col-md-2">'+
+'<p>Other Interest</br>'+
+'<p><a href="#">USA.gov</a></br>'+
+'<a href="#">Data.gov</a></br>'+
+'<a href="#">Recovery.gov</a></br>'+
+'<a href="#">Whitehouse.gov</a></p>'+
+'</div>'+
+'<div class="col-md-2">'+
+'<p>&nbsp;</p>'+
+'</div>'+
+'<div class="col-md-4 footerbkg">'+
+'<p style="color:#000">GSA/FAS Asset and Transportation Mgmt. Helpdesk</p>'+
+'<p>Hours of Operation<br>'+
+'8:00 AM - 7:00 PM EST<br> '+
+'Tel: 1-866-472-6711<br>'+
+'Email: gsadrivethruhelp@gsa.gov</p>'+
+'</div>'+
+'</div>';
+document.write (html);
+}
